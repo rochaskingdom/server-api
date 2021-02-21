7c67c0898379716ee74e75a2a89c3fa5dbc739b6
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-02-21 16:30:43 -0300

    Carregando textos - Parte 1 - Servidor

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/http_client', { useNewUrlParser: true,
-    useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/http_client', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const myLogger = function (req, res, next) {
     console.log(req.body);
@@ -20,12 +22,56 @@ app.use(myLogger);
 
 app.get('/products', (req, res) => {
     Product.find({}).lean()
-    .exec((err, prods) => {
+        .exec((err, prods) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(prods);
+        });
+});
+
+app.get('/productserr', (req, res) => {
+    setTimeout(() => {
+        res.status(500).send({
+            msg: 'Error message from the server'
+        });
+    }, 2000);
+});
+
+app.get('/productsdelay', (req, res) => {
+    setTimeout(() => {
+        Product.find({}).lean()
+            .exec((err, prods) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.status(200).send(prods);
+            });
+    }, 2000);
+});
+
+app.get('/products_ids', (req, res) => {
+    Product.find({}).lean()
+        .exec((err, prods) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(prods.map(p => p._id));
+        });
+});
+
+app.get('/products/name/:id', (req, res) => {
+    const id = req.params.id;
+    Product.findById(id, (err, prod) => {
         if (err) {
             res.status(500).send(err);
+        } else if (!prod) {
+            res.status(404).send({});
+        } else {
+            res.status(200).send(prod);
         }
-       res.status(200).send(prods);
     });
-});
+})
+;
 
 app.listen(3000);
